c122e0b93b7ecbc48d3067b4916e6626fe9aeb80
Tom Warner <tom.p.a.warner@gmail.com>
2019-02-26 17:15:28 +0000

    Initial commit

---
+++ clusterbank_maker.py
@@ -0,0 +1,39 @@
+import numpy as np
+import os
+import pickle
+
+
+home_dir = "/home/tom/d/Open_ephys_testing_ground_data/190222/2019-02-22_14-15-45"
+
+good_clusters = [2, 5, 13, 14, 20, 37, 47, 51, 65, 66]
+
+header = {'home_dir':home_dir, 'date':190222, 'good_clusters':good_clusters}
+
+times = np.load(os.path.join(home_dir, 'spike_times.npy'))
+clusters = np.load(os.path.join(home_dir, 'spike_clusters.npy'))
+spike_templates = np.load(os.path.join(home_dir, 'spike_templates.npy'))
+templates = np.load(os.path.join(home_dir, 'templates.npy'))
+templates_ind = np.load(os.path.join(home_dir, 'templates_ind.npy'))
+chan_positions = np.load(os.path.join(home_dir, 'channel_positions.npy'))
+chan_map = np.load(os.path.join(home_dir, 'channel_map.npy'))
+
+chan_dict = dict(zip(chan_map.T[0]+1, chan_positions))
+
+
+units = {}
+
+for cluster in good_clusters:
+	c_times = times[(clusters==cluster)]
+	c_template_ids = spike_templates[(clusters==cluster)]
+	c_templates = templates[np.unique(c_template_ids)]
+	max_chan = 0
+	template_maxes = {}
+	for clus_temp in c_templates:
+		chan_sums = [abs(sum(j)) for j in clus_temp.T]
+		template_maxes[max(chan_sums)] = np.argmax(chan_sums)
+	chan_max = template_maxes[max(template_maxes.keys())]
+	units[cluster] = {'max_chan':chan_max, 'unique_temps_ids':np.unique(c_template_ids), 'times':c_times, 'template_ids':c_template_ids, 'templates':c_templates}
+
+cluster_dict = {'header':header, 'units':units, 'chan_dict':chan_dict}
+
+pickle.dump(cluster_dict, open(os.path.join(home_dir, 'clusterbank.pkl'), 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
---
+++ plot_cluster_spikes.py
@@ -0,0 +1,35 @@
+import numpy as np
+import openephys as oe
+import pickle
+import os 
+import matplotlib.pyplot as plt
+
+home_dir = '/home/tom/d/Open_ephys_testing_ground_data/190222/2019-02-22_14-15-45'
+
+clusterbank = pickle.Unpickler(open(os.path.join(home_dir, 'clusterbank.pkl'), 'rb')).load()
+chan_dict = clusterbank['chan_dict']
+clusters = clusterbank['units']
+
+x = np.arange(-1, 2, 1/30)
+
+for cluster_num in clusters:
+	cluster = clusters[cluster_num]
+	max_chan = cluster['max_chan']
+	chan = oe.loadContinuous2(os.path.join(home_dir, '100_CH%d.continuous'% max_chan))
+	data =  chan['data']
+	times = cluster['times']
+	spikes = []
+	for index, time in enumerate(times):
+		spikes.append(data[int(time-30):int(time+60)] - np.median(data[int(time-30):int(time+60)]))
+		if index % 5000 == 0:
+			print(index)
+
+	for spike in spikes:
+		plt.plot(x, spike, color='gray')
+	avg_spike = np.mean(spikes, axis=0)
+	plt.plot(x, avg_spike)
+	plt.xlabel('Time (ms)')
+	plt.ylabel('Voltage ($\mu V$)')
+	plt.title('Cluster avg spike %d, chan: %d' % (cluster_num, max_chan))
+	plt.savefig(os.path.join(home_dir, 'unit_plots/cluster_%d.png' % cluster_num))
+	plt.clf()
