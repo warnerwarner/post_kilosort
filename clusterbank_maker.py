f265d441c7c705125a24af0dc10b035a268348b7
Tom Warner <tom.p.a.warner@gmail.com>
2019-03-29 12:53:32 +0000

    Comments and import openephys

--- clusterbank_maker.py
+++ clusterbank_maker.py
@@ -6,10 +6,11 @@ import csv
 
 home_dir = "/home/camp/warnert/working/Recordings/190325/2019-03-25_16-57-37"
 
+# Read in the cluster classifications
 cluster_tsv = os.path.join(home_dir, 'cluster_group.tsv')
-
 tsv_read = csv.reader(open(cluster_tsv, 'r'), delimiter='\t')
 
+# Hold the good, bad and ugly clusters
 good_clusters = []
 mua_clusters = []
 noise_clusters = []
@@ -21,11 +22,14 @@ for i in tsv_read:
 	elif i[1] == 'noise':
 		noise_clusters.append(int(i[0]))
 
-print(good_clusters)
 
+# Concatenate all the clusters together for the loop
 all_clusters= np.concatenate((good_clusters, mua_clusters, noise_clusters))
+
+# Make a nice little header
 header = {'home_dir':home_dir, 'date':190325, 'good_clusters':good_clusters, 'mua_clusters':mua_clusters, 'noise_clusters':noise_clusters}
 
+# Load in all the post kilosort stuff
 times = np.load(os.path.join(home_dir, 'spike_times.npy'))
 clusters = np.load(os.path.join(home_dir, 'spike_clusters.npy'))
 spike_templates = np.load(os.path.join(home_dir, 'spike_templates.npy'))
@@ -33,16 +37,14 @@ templates = np.load(os.path.join(home_dir, 'templates.npy'))
 templates_ind = np.load(os.path.join(home_dir, 'templates_ind.npy'))
 chan_positions = np.load(os.path.join(home_dir, 'channel_positions.npy'))
 chan_map = np.load(os.path.join(home_dir, 'channel_map.npy'))
-
 chan_dict = dict(zip(chan_map.T[0]+1, chan_positions))
 
-
+# Dictionary to hold the actual information for the clusters
 good_units = {}
 noise_units = {}
 mua_units = {}
 
-print(all_clusters)
-
+# Run through all the clusters
 for cluster in all_clusters:
 	c_times = times[(clusters==cluster)]
 	c_template_ids = spike_templates[(clusters==cluster)]
@@ -50,12 +52,11 @@ for cluster in all_clusters:
 	max_chan = 0
 	template_maxes = {}
 	for clus_temp in c_templates:
+		# Used only if a cluster comes from a merge
 		chan_sums = [abs(sum(j)) for j in clus_temp.T]
-		print(len(chan_sums))
 		template_maxes[max(chan_sums)] = np.argmax(chan_sums)
 	chan_max = template_maxes[max(template_maxes.keys())]
 	if cluster in good_clusters:
-		print('here')
 		good_units[cluster] = {'max_chan':chan_max, 'unique_temps_ids':np.unique(c_template_ids), 'times':c_times, 'template_ids':c_template_ids, 'templates':c_templates}
 	elif cluster in mua_clusters:
 		mua_units[cluster] = {'max_chan':chan_max, 'unique_temps_ids':np.unique(c_template_ids), 'times':c_times, 'template_ids':c_template_ids, 'templates':c_templates}
@@ -63,7 +64,8 @@ for cluster in all_clusters:
 		noise_units[cluster] = {'max_chan':chan_max, 'unique_temps_ids':np.unique(c_template_ids), 'times':c_times, 'template_ids':c_template_ids, 'templates':c_templates}
 
 
-
+# Turn all the unit dicts into one big dict
 cluster_dict = {'header':header, 'good_units':good_units, 'mua_units':mua_units, 'noise_units':noise_units, 'chan_dict':chan_dict}
 
+# And ump it all
 pickle.dump(cluster_dict, open(os.path.join(home_dir, 'clusterbank.pkl'), 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
--- tensor_maker.py
+++ tensor_maker.py
@@ -3,6 +3,7 @@ import pickle
 import os
 import psutil
 import pandas
+import openephys as oe
 
 available_cpu_count = len(psutil.Process().cpu_affinity())
 os.environ["MKL_NUM_THREADS"] = str(available_cpu_count)
@@ -22,29 +23,31 @@ clusterbank = pickle.Unpickler(open(oe.path.join(home_dir, 'clusterbank.pkl'), '
 clusters = clusterbank['good_units']
 trial_channel = oe.loadContinuous2(home_dir, '100_CH1.continuous')
 
-
+# Make a bin response which are 0 for when theres no spike and 1 for when there is
 bin_response = np.zeros([len(clusters), len(trial_channel['data'])])
 
+# Change all the spike times to 1 in bin_response
 for index, c in enumerate(clusters):
 	cluster = clusters[c]
 	times = cluster['times']
 	bin_response[index][time] = 1
 
+# Make a rolling sum of the spikes
 rolling_resp = pandas.DataFrame(bin_response)
 rolling_resp = rolling_resp.T 
 rolling_resp.colums = [c for c in clusters]
-
 spike_resps = rolling_resp.rolling(window = window).sum()
 spike_resp_array = np.array(spike_resps)
 trial_starts = np.fromfile(os.path.join(home_dir, 'trial_starts.npy'), dtype=np.int)
 
-
+# Make the trial_window e.g the length of the trial response 
 trial_window_size = 3*trial_length*fs
 
-trial_times = [j for i in trial_starts for j in  range(i-5*fs, i+2*trial_length*fs)]
+trial_times = [j for i in trial_starts for j in  range(i-trial_length*fs, i+2*trial_length*fs)]
 trial_resp_array = spike_resp_array.T[:, trial_times]
-tensor = np.zeros(len(clusters), trial_window_size, len(trial_starts))
 
+# Find the 
+tensor = np.zeros(len(clusters), trial_window_size, len(trial_starts))
 for i in range(len(trial_starts)):
 	tensor[:, :, i] = trial_resp_array[:, i*trial_window_size:(i+1)*trial_window_size]
 
