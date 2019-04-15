c35da9bff8d5799d3030cea72d0741e277d2ad2a
Tom Warner <tom.p.a.warner@gmail.com>
2019-04-15 15:13:02 +0100

    add comments and import sys

--- cluster_spike_plotter.py
+++ cluster_spike_plotter.py
@@ -3,6 +3,7 @@ Plots all those nice cluster spike plots to compare and the spikes that are isol
 '''
 
 import os
+import sys
 import psutil
 import numpy as np
 import matplotlib.pyplot as plt
@@ -127,31 +128,48 @@ def together_plot(data, spike_times, trial_starts, cluster_num, channel_num, out
 	plt.savefig(output_loc, dpi=300)
 
 
-home_dir = '/home/camp/warnert/working/Recordings/190410/2019-04-10_15-04-49'
-
-chan_map = np.load(os.path.join(home_dir, 'channel_map.npy'))
 
 
-clusterbank = pickle.Unpickler(open(os.path.join(home_dir, 'clusterbank.pkl'), 'rb')).load()
-good_clusters = clusterbank['good_units']
+if __name__ == '__main__':
+	#magic code
+	available_cpu_count = len(psutil.Process().cpu_affinity())
+	os.environ["MKL_NUM_THREADS"] = str(available_cpu_count)
 
-chosen_unit_index = list(sys.argv)[0]
-chosen_unit_index = int(chosen_unit_index)
-cluster_num = good_clusters.values[chosen_unit_index]
+	# Location of data
+	home_dir = '/home/camp/warnert/working/Recordings/190410/2019-04-10_15-04-49'
 
-cluster= good_clusters[cluster_num]
+	# Location of phy channel map
+	chan_map = np.load(os.path.join(home_dir, 'channel_map.npy'))
 
-spike_times = cluster['times']
+	# Clusterbank and good clusters
+	clusterbank = pickle.Unpickler(open(os.path.join(home_dir, 'clusterbank.pkl'), 'rb')).load()
+	good_clusters = clusterbank['good_units']
 
+	# Taking the chosen unit from the index of the good clusters
+	chosen_unit_index = list(sys.argv)[0]
+	chosen_unit_index = int(chosen_unit_index)
+	cluster_num = good_clusters.values[chosen_unit_index]
 
-trial_starts = np.fromfile(os.path.join(home_dir, 'trial_starts.npy'), dtype=np.int)
+	# Open the single cluster info
+	cluster= good_clusters[cluster_num]
+	spike_times = cluster['times']
 
-full_trials = trial_starts[1:113] # Missed first one and mouse died during final iteration kept the first of the final iteration to keep repeats equal
-channel_num = cluster['max_chan']
-channel_num = int(chan_map[channel_num])
-data = oe.loadContinuous2(os.path.join(home_dir, '100_CH%d.continuous' % channel_num + 1))['data']
-output_dir = os.path.join(home_dir, 'unit_plots')
-if not os.path.isdir(output_dir):
-	os.mkdir(output_dir)
+	# Load the trial starts
+	trial_starts = np.fromfile(os.path.join(home_dir, 'trial_starts.npy'), dtype=np.int)
 
-together_plot(data, spike_times, trial_starts, cluster_num, channel_num+1, os.path.join(output_dir, '%d_cluster.png' % cluster_num))
+	full_trials = trial_starts[1:113] # Missed first one and mouse died during final iteration kept the first of the final iteration to keep repeats equal
+
+	# Map the channel number using the channel map
+	channel_num = cluster['max_chan']
+	channel_num = int(chan_map[channel_num])
+
+	#Load all the data
+	data = oe.loadContinuous2(os.path.join(home_dir, '100_CH%d.continuous' % channel_num + 1))['data']
+
+	#Make the output if it doesn't exit
+	output_dir = os.path.join(home_dir, 'unit_plots')
+	if not os.path.isdir(output_dir):
+		os.mkdir(output_dir)
+
+	#Do all the stuff
+	together_plot(data, spike_times, full_trials, cluster_num, channel_num+1, os.path.join(output_dir, '%d_cluster.png' % cluster_num))
