05864f1a3f581331b6f77423f8a53229d1eea925
Tom Warner <tom.p.a.warner@gmail.com>
2019-04-30 20:48:18 +0100

    update do_it_all to make cluster graphs

--- do_it_all.py
+++ do_it_all.py
@@ -5,21 +5,33 @@ import sys
 from cluster_spike_plotter import *
 from clusterbank_maker import *
 
-home_dir = ''
+home_dir = '/home/camp/warnert/working/Recordings/190410/2019-04-10_15-04-49'
 
 num_of_chans= 64
 bit_volts=0.195
 
-clusterbank = make_clusterbank_full(home_dir, num_of_chans)
+clusterbank = pickle.Unpickler(open(os.path.join(home_dir, 'clusterbank_full.pkl'), 'rb')).load()
 
-data_loc = os.path.join(home_dir, '100_CHs.dat')
+
 trial_starts = np.fromfile(os.path.join(home_dir, 'trial_starts.npy'), dtype=np.int)
+window_size = 100
+bin_size = 1
+trial_length = 5
+#data = oe.loadContinuous2(os.path.join(home_dir, '100_CH1.continuous'))['data']
 
 for cluster_num in clusterbank['good_units']:
 	cluster = clusterbank['good_units'][cluster_num]
 	data_chan = int(cluster['max_chan'] +1 )
 	spike_times = cluster['times']
  	data = oe.loadContinuous2(os.path.join(home_dir, '100_CH%d.continuous' % data_chan))['data']
-	x, cluster_spikes = find_cluster_spikes(data, spike_times)
+	spike_x, cluster_spikes = find_cluster_spikes(data, spike_times)
+	
+	cluster_times = [int(i[0]) for i in cluster['times']]
+	cluster_times = np.array(cluster_times)
 
-	trial_spike_times = find_trial_spike_times(trial_starts)	
+	bins = spike_correlation(cluster_times)
+	trial_spike_times = find_trial_spike_times(trial_starts, cluster['times'])
+	recording_len = len(data)
+	output_loc = os.path.join(home_dir, 'cluster_plots', 'cluster_%d.png' % cluster_num)
+	together_plot(spike_x, cluster_spikes, bins, window_size, bin_size, trial_spike_times, trial_length, cluster_num, cluster, recording_len, output_loc)
+	plt.clf()
